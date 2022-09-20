3f78baaf05d1f9a55d1011c21d25f62ecf01d8b9
ola20sarumi <61728654+ola20sarumi@users.noreply.github.com>
2022-09-20 15:53:29 +0300

    json server,http,services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
 import {MatRadioModule} from '@angular/material/radio';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -43,7 +45,10 @@ import {MatRadioModule} from '@angular/material/radio';
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatRadioModule
+    MatRadioModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-dialog',
@@ -8,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 export class DialogComponent implements OnInit {
 
   freshnessList = ["Brand New", "Second Hand", "Refurbished"]
-  constructor() { }
+  productForm !: FormGroup;
+  constructor(private formBuilder : FormBuilder, private api : ApiService) { }
 
   ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      productName : ['',Validators.required],
+      category : ['',Validators.required],
+      freshness : ['',Validators.required],
+      price : ['',Validators.required],
+      comment : ['',Validators.required],
+      date : ['',Validators.required],
+    })
   }
-
+  addProduct(){
+    if(this.productForm.valid){
+      this.api.postProduct(this.productForm.value)
+      .subscribe({
+        next:(res)=>{
+          alert("Product added successfully")
+        },
+        error:()=>{
+          alert("Unable to add product")
+        }
+      })
+    }
+  }
 }
